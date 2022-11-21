8cce623032b6596ddf78dae789ecd46405e759a6
André Flores <andrejesusferflores@gmail.com>
2022-11-21 18:30:07 +0000

    Add boostrapping

--- src/backend/models/node.py
+++ src/backend/models/node.py
@@ -4,43 +4,30 @@ import sys
 
 from kademlia.network import Server
 
+from utils.node_utils import read_config_file
+
 # python get.py <bootstrap node> <bootstrap port> <key>
 # get.py 1.2.348 3244 SpecialKey
 
 
 class Node:
-    def __init__(self, ip : str, port : int, name : str) -> None:
+    def __init__(self, ip : str, port : int, name : str, bootstrap_file : str) -> None:
         self.setup_logger()
 
         self.ip = ip
         self.port = port
         self.name = name
 
+        self.connected_nodes = read_config_file(bootstrap_file)
+
         self.server = Server()
         
-        asyncio.run(self.testListen())
+        asyncio.run(self.server.listen(self.port))
+        asyncio.run(self.server.bootstrap(self.connected_nodes))
 
     def setup_logger(self):
         logging.basicConfig(level=logging.DEBUG)
         logging.getLogger("kademlia").setLevel(logging.INFO)
-
-    async def testListen(self):
-        if(self.name != "hello"):
-            self.name = "hello"
-        
-        await self.server.listen(self.port)
-        
-        bootstrap_node = (self.ip, self.port)
-        await self.server.bootstrap([bootstrap_node])
-        #await self.server.set(self.name, "Hello World")
-        
-        result = None
-        stay = True
-        result = await self.server.get(self.name)
-        #result = await self.server.get(self.name)
-        print("Result: {}".format(result))
-        self.server.stop()
-
 
 def main():
     node = Node(sys.argv[1], int(sys.argv[2]), sys.argv[3])
---
+++ src/backend/utils/node_utils.py
@@ -0,0 +1,14 @@
+
+def read_config_file(config_file_path):
+    """Reads the config file and returns the config dictionary.
+
+    Args:
+        config_file_path: Path to the config file.
+
+    Returns:
+        List of tuples containing the ip and port of the bootstrap nodes.
+    """
+    with open(config_file_path, 'r') as config_file:
+        config = config_file.readlines()
+
+    return [{'ip' : ip, 'port' : int(port.replace('\n', ''))} for ip, port in [tuple(line.split(' ')) for line in config]]
